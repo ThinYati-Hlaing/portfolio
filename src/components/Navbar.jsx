cfb701685c091791d2e9ce49e38887733f2c5a6d
thinyati-hlaing <thinyatihlaing6@gmail.com>
2024-04-06 15:40:02 +0630

    icon change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,10 +9,10 @@ const Navbar = () => {
         <nav className='fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50'>
             <div className=' container mx-auto'>
                 <div className='w-full gradient h-[70px] border backdrop-blur-2xl rounded-full max-w-[460px] mx-auto px-5 flex justify-between items-center text-2xl text-fuchsia-400' >
-                    <Link to='home'  smooth={true} spy={true} offset={-300} className='cursor-pointer active w-[60px] h-[60px] flex justify-center items-center'>
+                    {/* <Link to='home'  smooth={true} spy={true} offset={-300} className='cursor-pointer active w-[60px] h-[60px] flex justify-center items-center'>
                         <BiHomeAlt />
-                    </Link>
-                    <Link to='about'  smooth={true} spy={true} className='cursor-pointer active w-[60px] h-[60px] flex justify-center items-center'>
+                    </Link> */}
+                    <Link to='home'  smooth={true} spy={true} offset={-300} className='cursor-pointer active w-[60px] h-[60px] flex justify-center items-center'>
                         <BiUser />
                     </Link>
                     <Link to='skill'  smooth={true} spy={true} className='cursor-pointer active w-[60px] h-[60px] flex justify-center items-center'>
