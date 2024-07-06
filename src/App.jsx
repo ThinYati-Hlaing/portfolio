e0bec02fde11664cfed25b974ec78f629da5edcd
thinyati-hlaing <thinyatihlaing6@gmail.com>
2024-07-07 00:06:52 +0630

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import Main from "./components/Main";
+import Hero from "./components/Hero";
+import Contact from "./components/Contact";
+import Skill from "./components/Skill";
+import Projects from "./components/Projects";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <>
-      <Main />
-    </>
-  )
-
+    <div className="bg" >
+        <Header/>
+        <Hero/>
+        <Navbar/>
+        <Skill/>
+        <Projects/>
+        <Contact/>
+    </div>
+)
 }
 
 export default App;
