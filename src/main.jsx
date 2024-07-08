81245eb768826c0bd4d6c6385888476db0b9f625
thinyati-hlaing <thinyatihlaing6@gmail.com>
2024-07-08 22:31:20 +0630

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter as Router } from 'react-router-dom'
-import { ChakraProvider } from '../node_modules/.vite/deps/@chakra-ui_react.js'
+import { ChakraProvider } from '@chakra-ui/react'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
